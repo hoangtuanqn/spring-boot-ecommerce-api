594609da0a58c871e53a22a8abd37f548697d93f
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-06-23 21:01:21 +0700

    init

---
+++ src/main/java/java/local/mstsoftware/MstsoftwareApplication.java
@@ -0,0 +1,13 @@
+package java.local.mstsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstsoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/local/mstsoftware/MstsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package java.local.mstsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
