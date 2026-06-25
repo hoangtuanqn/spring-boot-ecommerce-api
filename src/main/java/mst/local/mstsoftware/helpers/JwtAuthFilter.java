dfa7429aded5cae7bdb970805c03b0d90df5c474
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-06-26 02:57:09 +0700

    feat: jwt auth filter chain

---
+++ src/main/java/mst/local/mstsoftware/helpers/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package mst.local.mstsoftware.helpers;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import mst.local.mstsoftware.services.JwtService;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // 1. Lấy token từ header
+        String authHeader = request.getHeader("Authorization");
+
+        // 2. Không có token hoặc không đúng format -> bỏ qua, đi tiếp
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 3. Extract Token
+        String token = authHeader.substring(7);
+        String email = jwtService.extractEmail(token);
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+            if (jwtService.isTokenValid(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // truy xuất metadata
+                                                                                                  // request
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
