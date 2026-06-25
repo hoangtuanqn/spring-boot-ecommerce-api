83e3cba32760d982ac0beabacb0a5e4caad137e0
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-06-26 02:56:40 +0700

    feat: user resource

--- src/main/java/mst/local/mstsoftware/modules/users/resources/UserResource.java
+++ src/main/java/mst/local/mstsoftware/modules/users/resources/UserResource.java
@@ -3,18 +3,28 @@ package mst.local.mstsoftware.modules.users.resources;
 public class UserResource {
     private final Long id;
     private final String email;
+    private final String name;
 
     public UserResource(Long id, String email) {
-        this.id = id;
-        this.email = email;
+        this(id, email, "");
     }
 
-    public Long getId(Long id) {
+    public UserResource(Long id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public Long getId() {
         return id;
     }
 
     public String getEmail() {
         return email;
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
