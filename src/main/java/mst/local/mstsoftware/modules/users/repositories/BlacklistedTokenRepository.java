ff1276a5ca28d56ae3658e6567bd498f3f7d8ce1
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-09 09:11:48 +0700

    feat: interface BlacklistedTokenRepository

---
+++ src/main/java/mst/local/mstsoftware/modules/users/repositories/BlacklistedTokenRepository.java
@@ -0,0 +1,17 @@
+package mst.local.mstsoftware.modules.users.repositories;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import mst.local.mstsoftware.modules.users.entities.BlacklistedToken;
+
+public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, Long> {
+    boolean existsByToken(String token);
+
+    void deleteByExpiryDateBefore(LocalDateTime date);
+
+    List<BlacklistedToken> findByUserId(Long userId);
+
+}
