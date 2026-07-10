f48979d895e25554a4716dda2341cf84d524d22e
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-11 02:04:56 +0700

    feat: implement black list token

--- src/main/java/mst/local/mstsoftware/modules/users/services/impl/BlacklistService.java
+++ src/main/java/mst/local/mstsoftware/modules/users/services/impl/BlacklistService.java
@@ -7,34 +7,36 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mst.local.mstsoftware.helpers.TokenHashUtil;
 import mst.local.mstsoftware.modules.users.entities.BlacklistedToken;
 import mst.local.mstsoftware.modules.users.repositories.BlacklistedTokenRepository;
+import mst.local.mstsoftware.modules.users.services.interfaces.BlacklistServiceInterface;
 import mst.local.mstsoftware.services.JwtService;
 
 @Service
 @Slf4j
 @AllArgsConstructor
-public class BlacklistService {
+public class BlacklistService implements BlacklistServiceInterface {
     private BlacklistedTokenRepository repository;
     private JwtService jwtService;
+    private TokenHashUtil tokenHashUtil;
 
+    @Transactional
     public void blacklistToken(String token) {
         if (isTokenBlackList(token)) {
             return;
         }
+        String tokenHash = tokenHashUtil.hash(token);
         Claims claims = jwtService.extractAllClaims(token);
         Long userId = jwtService.extractUserId(token);
         Date expiration = claims.getExpiration();
 
         BlacklistedToken entity = new BlacklistedToken();
         entity.setUserId(userId);
-        entity.setToken(token);
-        // log.info("Origin: " + expiration);
-        // log.info("Instant: " + expiration.toInstant());
-        // log.info("At zone: " +
-        // expiration.toInstant().atZone(ZoneId.systemDefault()));
+        entity.setTokenHash(tokenHash);
         entity.setExpiryDate(
                 expiration.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
 
@@ -42,9 +44,11 @@ public class BlacklistService {
     }
 
     public boolean isTokenBlackList(String token) {
-        return repository.existsByToken(token);
+        String tokenHash = tokenHashUtil.hash(token);
+        return repository.existsByTokenHash(tokenHash);
     }
 
+    @Transactional
     public void blacklistAllUserTokens(Long userId, List<String> activeTokens) {
         activeTokens.forEach(this::blacklistToken);
     }
