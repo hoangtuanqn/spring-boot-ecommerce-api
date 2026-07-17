082e253606ff0d213f3b96c389feea57853c1f88
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-18 02:52:47 +0700

    feat: implement blacklist redis service

--- src/main/java/mst/local/mstsoftware/modules/users/services/impl/BlacklistRedisService.java
+++ src/main/java/mst/local/mstsoftware/modules/users/services/impl/BlacklistRedisService.java
@@ -1,4 +1,28 @@
 package mst.local.mstsoftware.modules.users.services.impl;
 
-public class BlacklistRedisService {
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mst.local.mstsoftware.modules.users.services.interfaces.BlacklistServiceInterface;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class BlacklistRedisService implements BlacklistServiceInterface {
+
+    private final RedisTemplate<String, Object> redis;
+
+    public void revoke(String jti, Instant expiresAt) {
+        long ttlSeconds = Duration.between(Instant.now(), expiresAt).getSeconds();
+        if (ttlSeconds <= 0) return;
+        redis.opsForValue().set(PREFIX + jti, "revoked", ttlSeconds);
+    }
+
+    public Boolean isRevoked(String jti) {
+        return redis.hasKey(PREFIX + jti);
+    }
 }
