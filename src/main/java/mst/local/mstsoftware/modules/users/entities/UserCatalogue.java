f6292c67a71cc8b166cbb7e5fcffd6bf21432e75
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-20 09:32:06 +0700

    feat: UserCatalogue entity

---
+++ src/main/java/mst/local/mstsoftware/modules/users/entities/UserCatalogue.java
@@ -0,0 +1,40 @@
+package mst.local.mstsoftware.modules.users.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user_catalogues")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserCatalogue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private Boolean publish;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreated() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdated() {
+        updatedAt = Instant.now();
+    }
+}
