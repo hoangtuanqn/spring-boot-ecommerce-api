6f984bafe6cbc0337f8f8f44222641aaeed511ce
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-20 09:34:39 +0700

    feat: user catalogue repository

---
+++ src/main/java/mst/local/mstsoftware/modules/users/repositories/UserCatalogueRepository.java
@@ -0,0 +1,9 @@
+package mst.local.mstsoftware.modules.users.repositories;
+
+import mst.local.mstsoftware.modules.users.entities.UserCatalogue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserCatalogueRepository extends JpaRepository<UserCatalogue, Long> {
+}
