f7de930f85344868a2eccae2d2ebdc25b1df867f
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-20 09:34:08 +0700

    feat: User Catalogues controller and DTO

--- src/main/java/mst/local/mstsoftware/modules/users/controllers/UserCatalogueController.java
+++ src/main/java/mst/local/mstsoftware/modules/users/controllers/UserCatalogueController.java
@@ -1,5 +1,36 @@
 package mst.local.mstsoftware.modules.users.controllers;
 
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mst.local.mstsoftware.modules.users.entities.UserCatalogue;
+import mst.local.mstsoftware.modules.users.requests.UserCatagoue.CreateUserCatalogueRequest;
+import mst.local.mstsoftware.modules.users.resources.UserCatalogueResource;
+import mst.local.mstsoftware.modules.users.services.interfaces.UserCatalogueServiceInterface;
+import mst.local.mstsoftware.resources.ApiResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+@RequestMapping("/api/v1/user-catalogues")
 public class UserCatalogueController {
-    
+    private final UserCatalogueServiceInterface userCatalogueService;
+//    private final UserCa
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody CreateUserCatalogueRequest request) {
+        UserCatalogue userCatalogue = userCatalogueService.create(request);
+        UserCatalogueResource userCatalogueResource = UserCatalogueResource.builder()
+                .name(userCatalogue.getName())
+                .publish(userCatalogue.getPublish())
+                .build();
+        return
+                ResponseEntity.ok(ApiResource.success(userCatalogueResource, "Thêm user catalogue thành công!"));
+    }
 }
---
+++ src/main/java/mst/local/mstsoftware/modules/users/resources/UserCatalogueResource.java
@@ -0,0 +1,16 @@
+package mst.local.mstsoftware.modules.users.resources;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserCatalogueResource {
+    private final Long id;
+    private final String name;
+    private final Boolean publish;
+}
