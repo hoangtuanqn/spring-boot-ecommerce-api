27caa8baaf939ba165902d16706d9c36b95fc4b1
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-24 22:33:22 +0700

    feat: optimeze code

--- src/main/java/mst/local/mstsoftware/helpers/Helpers.java
+++ src/main/java/mst/local/mstsoftware/helpers/Helpers.java
@@ -8,4 +8,8 @@ public class Helpers {
             return defaultValue;
         }
     }
+
+    public static Integer parseIntSafe(String[] values, Integer defaultValue) {
+        return parseIntSafe((values != null && values.length > 0) ? values[0] : null, defaultValue);
+    }
 }
