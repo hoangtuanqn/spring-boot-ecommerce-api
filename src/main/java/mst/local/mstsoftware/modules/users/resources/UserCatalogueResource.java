dbd493149285ccc451f6f636e0cbb24153e5e089
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-25 11:46:58 +0700

    feat: remove method fromEntity

--- src/main/java/mst/local/mstsoftware/modules/users/resources/UserCatalogueResource.java
+++ src/main/java/mst/local/mstsoftware/modules/users/resources/UserCatalogueResource.java
@@ -2,16 +2,8 @@ package mst.local.mstsoftware.modules.users.resources;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import mst.local.mstsoftware.modules.users.entities.UserCatalogue;
 
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserCatalogueResource(Long id, String name, Integer publish) {
-    public static UserCatalogueResource fromEntity(UserCatalogue entity) {
-        return UserCatalogueResource.builder()
-                .id(entity.getId())
-                .name(entity.getName())
-                .publish(entity.getPublish())
-                .build();
-    }
 }
