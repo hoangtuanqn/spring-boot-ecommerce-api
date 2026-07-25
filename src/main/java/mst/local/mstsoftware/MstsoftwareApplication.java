ffddd7c8bc08c2f83b81551bb4857d805955d427
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-25 14:07:43 +0700

    feat: struct json response paginate

--- src/main/java/mst/local/mstsoftware/MstsoftwareApplication.java
+++ src/main/java/mst/local/mstsoftware/MstsoftwareApplication.java
@@ -2,10 +2,13 @@ package mst.local.mstsoftware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+// thêm cái này vào để trả json khi pagination ổn định hơn
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class MstsoftwareApplication {
 
     public static void main(String[] args) {
