f42eb794eea01406d848cca2c4fc0728b502fad6
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-25 13:00:50 +0700

    feat: validation data request delete batch

--- src/main/java/mst/local/mstsoftware/modules/users/requests/UserCatagoue/BatchDeleteRequest.java
+++ src/main/java/mst/local/mstsoftware/modules/users/requests/UserCatagoue/BatchDeleteRequest.java
@@ -1,6 +1,8 @@
 package mst.local.mstsoftware.modules.users.requests.UserCatagoue;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.util.List;
@@ -8,5 +10,8 @@ import java.util.List;
 @Data
 public class BatchDeleteRequest {
     @NotEmpty(message = "Không được để trống danh sách ids")
-    private List<Long> ids;
+    private List<
+            @NotNull(message = "Id không được phép để null!")
+            @Positive(message = "Id phải là số dương!")
+                    Long> ids;
 }
