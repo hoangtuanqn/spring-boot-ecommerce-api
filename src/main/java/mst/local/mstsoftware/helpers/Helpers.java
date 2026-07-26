e05f26016cf135e7acf47a5e92c1dc4bbb7049bf
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-26 15:29:14 +0700

    fix: bug method parseIntSafe

--- src/main/java/mst/local/mstsoftware/helpers/Helpers.java
+++ src/main/java/mst/local/mstsoftware/helpers/Helpers.java
@@ -3,7 +3,7 @@ package mst.local.mstsoftware.helpers;
 public class Helpers {
     public static Integer parseIntSafe(String value, Integer defaultValue) {
         try {
-            return (value != null && value.isEmpty()) ? Integer.parseInt(value) : defaultValue;
+            return (value != null && !value.isEmpty()) ? Integer.parseInt(value) : defaultValue;
         } catch (NumberFormatException e) {
             return defaultValue;
         }
