d1569c3994c4a84d3a141017f03cd4de747aefd5
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-31 11:11:02 +0700

    feat: add annotation @Repository

--- src/main/java/mst/local/mstsoftware/modules/users/repositories/RefreshTokenRepository.java
+++ src/main/java/mst/local/mstsoftware/modules/users/repositories/RefreshTokenRepository.java
@@ -8,10 +8,12 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByTokenHash(String token);
 
