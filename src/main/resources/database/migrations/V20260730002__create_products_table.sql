9bc0ea0aa169885d545c7ed622f1da8c7c095523
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-07-31 10:53:33 +0700

    feat: index category_id

--- src/main/resources/database/migrations/V20260730002__create_products_table.sql
+++ src/main/resources/database/migrations/V20260730002__create_products_table.sql
@@ -7,5 +7,6 @@ CREATE TABLE products (
     quantity INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+    INDEX idx_category_id (category_id)
 )
