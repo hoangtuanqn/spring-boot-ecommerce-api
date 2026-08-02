2d9cac3276ff082b6cc32b02acde6efe4a0d31ac
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-02 18:59:46 +0700

    feat: add item to cart controller

--- src/main/java/mst/local/mstsoftware/modules/order/controllers/CartController.java
+++ src/main/java/mst/local/mstsoftware/modules/order/controllers/CartController.java
@@ -1,16 +1,16 @@
 package mst.local.mstsoftware.modules.order.controllers;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import mst.local.mstsoftware.controllers.BaseController;
+import mst.local.mstsoftware.modules.order.requests.AddCartItemRequest;
 import mst.local.mstsoftware.modules.order.requests.CartResource;
 import mst.local.mstsoftware.modules.order.services.interfaces.CartServiceInterface;
 import mst.local.mstsoftware.modules.user.resources.CustomUserDetails;
 import mst.local.mstsoftware.resources.ApiResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -21,6 +21,11 @@ public class CartController extends BaseController {
     @GetMapping
     public ResponseEntity<ApiResource<CartResource>> getCart(@AuthenticationPrincipal CustomUserDetails userDetails) {
         return ok(cartService.getCart(userDetails.getId()), "Lấy giỏ hàng thành công!");
+    }
+
+    @PostMapping("/items")
+    public ResponseEntity<ApiResource<CartResource>> addItem(@Valid @RequestBody AddCartItemRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ok(cartService.addItem(userDetails.getId(), request), "Thêm vào giỏ hàng thành công!");
     }
 
 }
