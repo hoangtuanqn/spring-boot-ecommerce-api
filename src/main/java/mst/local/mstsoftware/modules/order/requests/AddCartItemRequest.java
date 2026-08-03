3e6d86c8b2149a971db7664fe68a2e488102b9e1
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-03 18:11:21 +0700

    feat: message validation

--- src/main/java/mst/local/mstsoftware/modules/order/requests/AddCartItemRequest.java
+++ src/main/java/mst/local/mstsoftware/modules/order/requests/AddCartItemRequest.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record AddCartItemRequest(
-        @NotNull Long productId,
-        @Positive Integer quantity
+        @NotNull(message = "Vui lòng chọn sản phẩm muốn thêm!") Long productId,
+
+        @Positive(message = "Số luợng phải lớn hơn 0!") Integer quantity
 ) {
 }
