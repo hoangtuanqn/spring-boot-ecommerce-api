cc8c63def861448a7cea2f01f6d0cd4dcff2343f
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-04 12:40:14 +0700

    fix: method findByNameWithPermissions, existsByName

--- src/main/java/mst/local/mstsoftware/modules/user/repositories/RoleRepository.java
+++ src/main/java/mst/local/mstsoftware/modules/user/repositories/RoleRepository.java
@@ -3,6 +3,8 @@ package mst.local.mstsoftware.modules.user.repositories;
 import mst.local.mstsoftware.modules.user.entities.Role;
 import mst.local.mstsoftware.modules.user.enums.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,13 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleType role);
+
+    @Query("""
+                SELECT r FROM Role r
+                LEFT JOIN FETCH r.permissions
+                WHERE r.name = :name
+            """)
+    Optional<Role> findByNameWithPermissions(@Param("name") String name);
+
+    boolean existsByName(String name);
 }
