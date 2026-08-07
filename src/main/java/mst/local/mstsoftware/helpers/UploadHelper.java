872cfb5f252b661cae3166abee44995b6938cc47
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-08 01:21:54 +0700

    feat: config max size upload and display image mvc

--- src/main/java/mst/local/mstsoftware/helpers/UploadHelper.java
+++ src/main/java/mst/local/mstsoftware/helpers/UploadHelper.java
@@ -1,6 +1,8 @@
 package mst.local.mstsoftware.helpers;
 
 public class UploadHelper {
+
+
     private static String getExtension(String fileName) {
         return fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
     }
