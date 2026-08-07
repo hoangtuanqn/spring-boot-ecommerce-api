99bae13b31b385c600ec499344d8dc51a99de631
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-08 04:15:55 +0700

    feat: add existingImages/newImages fields to update product request

--- src/main/java/mst/local/mstsoftware/modules/product/mappers/ProductMapper.java
+++ src/main/java/mst/local/mstsoftware/modules/product/mappers/ProductMapper.java
@@ -6,8 +6,22 @@ import mst.local.mstsoftware.modules.product.requests.CreateProductRequest;
 import mst.local.mstsoftware.modules.product.requests.UpdateProductRequest;
 import mst.local.mstsoftware.modules.product.resources.ProductResource;
 
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper extends BaseMapper<Product, ProductResource, CreateProductRequest, UpdateProductRequest> {
+
+    @Override
+    @Mapping(target = "images", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(UpdateProductRequest updateRequest, @MappingTarget Product entity);
+
+
+    default List<String> map(List<MultipartFile> files) {
+        return new ArrayList<>();
+    }
 }
