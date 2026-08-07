0fd8e858c17c8b2c32f2496b9491121e70029c33
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-08 02:07:23 +0700

    feat: upload multiple file helper

--- src/main/java/mst/local/mstsoftware/helpers/UploadHelper.java
+++ src/main/java/mst/local/mstsoftware/helpers/UploadHelper.java
@@ -1,9 +1,40 @@
 package mst.local.mstsoftware.helpers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
 public class UploadHelper {
+    @Value("${app.upload.dir}")
+    private String uploadDir;
 
+    public ArrayList<String> uploadFile(List<MultipartFile> files, String source) throws IOException {
+        var paths = new ArrayList<String>();
+        final String normalizedPath = (source.charAt(0) == '/' ? source : "/" + source).toLowerCase();
 
-    private static String getExtension(String fileName) {
+        Path dir = Paths.get(uploadDir + normalizedPath);
+        if (!Files.exists(dir)) Files.createDirectories(dir);
+        for (var file : files) {
+            String ext = getExtension(file.getOriginalFilename());
+            String fileName = UUID.randomUUID() + ext;
+            Files.copy(file.getInputStream(), dir.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+
+            paths.add("/uploads" + normalizedPath + "/" + fileName);
+        }
+        return paths;
+    }
+
+    private String getExtension(String fileName) {
         return fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
     }
 }
