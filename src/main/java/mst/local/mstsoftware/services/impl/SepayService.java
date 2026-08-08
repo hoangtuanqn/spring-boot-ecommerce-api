125b2c04dcb4751d70934d1976418d68985259c1
htuanqn <phamhoangtuanqn@gmail.com>
2026-08-08 20:09:12 +0700

    feat: add sepay service to fetch transactions

--- src/main/java/mst/local/mstsoftware/services/impl/SepayService.java
+++ src/main/java/mst/local/mstsoftware/services/impl/SepayService.java
@@ -1,4 +1,24 @@
 package mst.local.mstsoftware.services.impl;
 
-public class SepayService {
+import lombok.AllArgsConstructor;
+import mst.local.mstsoftware.config.SepayApiClient;
+import mst.local.mstsoftware.resources.SepayTransaction;
+import mst.local.mstsoftware.services.interfaces.SepayServiceInterface;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class SepayService implements SepayServiceInterface {
+    private final SepayApiClient sepayApiClient;
+
+    @Override
+    public List<SepayTransaction> getTransaction(String accountNumber, String fromtDate, String toDate, int limit) {
+        var response = sepayApiClient.getBankAccounts(accountNumber, fromtDate, toDate, limit);
+        if (response.getStatus() != 200) {
+            throw new RuntimeException(response.getError());
+        }
+        return response.getTransactions();
+    }
 }
--- src/main/java/mst/local/mstsoftware/services/interfaces/SepayServiceInterface.java
+++ src/main/java/mst/local/mstsoftware/services/interfaces/SepayServiceInterface.java
@@ -1,4 +1,9 @@
 package mst.local.mstsoftware.services.interfaces;
 
-public class SepayServiceInterface {
+import mst.local.mstsoftware.resources.SepayTransaction;
+
+import java.util.List;
+
+public interface SepayServiceInterface {
+    public List<SepayTransaction> getTransaction(String accountNumber, String fromtDate, String toDate, int limit);
 }
